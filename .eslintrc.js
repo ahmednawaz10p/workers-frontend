82b0990c9ebb4581ab3ed45767c9a78345ffd186
Ahmed Nawaz Khan <ahmed.nawaz@tenpearls.com>
2021-06-17 15:09:03 +0500

    added prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    "react-app",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+  },
+  overrides: [
+    {
+      files: ["**/*.ts?(x)"],
+      rules: {
+      },
+    },
+  ],
+};
